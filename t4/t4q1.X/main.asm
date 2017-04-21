c19f788e188173ecceef4c283f7e88205da554eb
Johnson Windows <johnsonlwh7@gmail.com>
2017-04-21 14:47:53 +0800

    done t4q1

--- t4/t4q1.X/main.asm
+++ t4/t4q1.X/main.asm
@@ -1,3 +1,4 @@
+#include <P16F84A.INC>
 ; TODO INSERT CONFIG CODE HERE USING CONFIG BITS GENERATOR
 
 RES_VECT  CODE    0x0000            ; processor reset vector
@@ -8,7 +9,30 @@ RES_VECT  CODE    0x0000            ; processor reset vector
 MAIN_PROG CODE                      ; let linker place main program
 
 START
-
-    GOTO $                          ; loop forever
+    STORAGE1 EQU 0x20
+    STORAGE2 EQU 0x21
+    COUNTER1 EQU d'10'
+    COUNTER2 EQU d'50'
+    
+    ; Initialize Port B
+    MOVLW 0x00
+    MOVWF PORTB
+ 
+    ; Move COUNTER1 to STORAGE1 (20)
+    MOVLW COUNTER1
+    MOVWF STORAGE1
+    ; Move COUNTER2 to STORAGE2 (21)
+    MOVLW COUNTER2
+    MOVWF STORAGE2
+    
+    LOOP1 MOVLW COUNTER2
+	  MOVWF STORAGE2
+	  LOOP2 COMF PORTB, 1		    ; toggle Port B
+		DECF STORAGE2, 1
+		BNZ LOOP2
+		DECF STORAGE1, 1
+		BNZ LOOP1
+    
+	
 
     END
