2b2450642a6c9ec00c9e8c616ccbd3eb8c4410f8
Johnson Windows <johnsonlwh7@gmail.com>
2017-04-21 16:05:30 +0800

    done building in q2,q3 and added q4

--- t4/t4q4.X/main.asm
+++ t4/t4q4.X/main.asm
@@ -1,4 +1,3 @@
-#include <P16F84A.INC>
 ; TODO INSERT CONFIG CODE HERE USING CONFIG BITS GENERATOR
 
 RES_VECT  CODE    0x0000            ; processor reset vector
@@ -9,19 +8,8 @@ RES_VECT  CODE    0x0000            ; processor reset vector
 MAIN_PROG CODE                      ; let linker place main program
 
 START
-    MOVLW D'100'
-    MOVWF MYREG
-    HERE NOP
-	 NOP
-	 NOP
-	 NOP
-	 NOP
-	 DECFSC MYREG, 1	    ;
-	 GOTO HERE
-    GOTO $			    ; infinit loop
-    END
     
-    ; NOP time = T * prescaling factor
-    ; NOP time = 1/4Mhz * 4
-    ; NOP time = 1 micro s
-    ; Total NOP = 5 micro s
+    
+    GOTO $                          ; loop forever
+
+    END
