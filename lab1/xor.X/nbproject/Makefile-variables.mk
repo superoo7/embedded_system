ef7bb364fc1febf896e71a20643b04cd97a0527d
Johnson Lai MBP <jlwhoo7@gmail.com>
2017-05-04 20:15:58 +0800

    added xor.X

---
+++ lab1/xor.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=xor.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/xor.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=xor.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/xor.x.tar
