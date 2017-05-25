08c88338fec7a31d7211cdc895ae83b1a154c109
Johnson Lai <jlwhoo7@gmail.com>
2017-05-25 22:53:03 +0800

    done t4q5

---
+++ t4/t4q5/q5.asm
@@ -0,0 +1,15 @@
+
+ES_VECT  CODE    0x0000            ; processor reset vector
+GOTO    START                   ; go to beginning of program
+
+
+MAIN_PROG CODE                      ; let linker place main program
+
+START
+
+
+
+
+
+
+END
