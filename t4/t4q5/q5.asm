b71c57735c6753e56e617432dd88242d037904c9
Johnson Lai <jlwhoo7@gmail.com>
2017-05-25 22:53:25 +0800

    Some tweaks on t4q5

--- t4/t4q5/q5.asm
+++ t4/t4q5/q5.asm
@@ -1,5 +1,8 @@
+; an assembly language program to for toggling a specific pin RB0 of Port B with a time delay of 1 second
 
-ES_VECT  CODE    0x0000            ; processor reset vector
+#include <P16F84A.INC>
+
+RES_VECT  CODE    0x0000            ; processor reset vector
 GOTO    START                   ; go to beginning of program
 
 
@@ -7,9 +10,31 @@ MAIN_PROG CODE                      ; let linker place main program
 
 START
 
+	BCF STATUS, RP0              ; SELECT BANK 0 (PORT B)
+	CLRF PORTB									 ; Initialize PORT B 
+
+	
+
+MAINPROGRAM
+	COMF PORTB, 1
+	CALL DELAY
+	GOTO MAINPROGRAM						
 
 
-
-
-
+DELAY
+    MOVLW D'6'
+	  MOVWF COUNTERC
+	  MOVLW D'24'
+	  MOVWF COUNTERB
+	  MOVLW D'168'
+	  MOVWF COUNTERA
+	 LOOP
+	  DECFSZ COUNTERA,1
+	  GOTO LOOP
+	  DECFSZ COUNTERB,1
+	  GOTO LOOP
+	  DECFSZ COUNTERC,1
+	  GOTO LOOP  
+	 RETURN
+	
 END
